4c9b831b1ac7dcdc5f73424529db94e4ad2d9af0
Tre Logan <tr3.logan@gmail.com>
2020-06-11 16:48:42 -0400

    Lesson 03

---
+++ notes/lesson03-event-driven-progarmming.md
@@ -0,0 +1,3 @@
+# Lesson 03 - Event Driven Programming
+### SDL_PollEvent
+* What SDL_PollEvent does is take the most recent event from the event queue and puts the data from the event into the SDL_Event we passed into the function.
--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,31 @@ int main(int argc, char *argv[]) {
         // Load media
         if (!loadMedia()) {
             printf("Failed to load media!\n");
-        } else {
-            // Apply the image
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-            // Update the surface
-            SDL_UpdateWindowSurface(gWindow);
-            // Wait two seconds
-            SDL_Delay(2000);
+            exit(0);
+        }
+
+        // Apply the image
+        SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+        // Update the surface
+        SDL_UpdateWindowSurface(gWindow);
+
+        // Main loop flag
+        bool quit = false;
+
+        // Event handler
+        SDL_Event e;
+
+        while (!quit) {
+            // Handle events on queue
+            while (SDL_PollEvent(&e) != 0) {
+                // User requests quit
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
         }
     }
+
     // Free resources and close SDL
     close();
 
